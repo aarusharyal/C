f5159121078dc972450396712bc65a5b3541e2c9
aarusharyal <249310662+aarusharyal@users.noreply.github.com>
2026-01-30 11:49:56 +0545

    Add files via upload

---
+++ sumof2numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int main(){ 
+int x,y,z;
+printf("enter the first number"); 	
+//Asking the user for the first number.
+scanf("%d",&x); 					
+//Putting the user input in the address of variable x.
+printf("enter the second number");	
+//Asking the user for the second number.
+scanf("%d",&y); 					
+//Putting the user input in the address of variable y.
+z=x+y;								
+//Pefroming add operation and storing the value in the variable z.
+printf("sum of two numbers is :%d",z);
+//Displaying the final result on the terminal.
+ return 0;
+ }
