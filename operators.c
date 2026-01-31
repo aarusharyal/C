2c1e283d90255e23095650f954fb137e826ec541
aarusharyal <249310662+aarusharyal@users.noreply.github.com>
2026-01-31 20:08:29 +0545

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main() {
+  int x=5; x++; 			// post increment operater 
+  printf("x= %d\n",x); ++x; //pre increment operater 
+  printf("x= %d\n",x); x--; //post decrement operater 
+  printf("x= %d\n",x); --x; //pre decrement operater 
+  printf("x= %d\n",x); 
+  return 0; 
+  }
+
+
