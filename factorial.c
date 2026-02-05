b7abf64ca57cbae54435dd8f9448a3cff1c2f159
aarusharyal <aryalaarush203@gmail.com>
2026-02-05 23:03:34 +0545

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int n,x=1,fact;
+	// Initializing the variables n,x,fact
+	printf("Enter the number:");
+	// Asking the user for the number
+	scanf ("%d",&n);
+	// Saving the number in the variable n
+	while (n!=0)
+	// Checking if the variable is 0 or not
+	{
+	fact=n*b;
+	x=n-1;
+	// Calculating the factorial
+	}
+	printf("Factorial:%d",&fact);
+	// Displaying the final output
+	return 0;
+}
