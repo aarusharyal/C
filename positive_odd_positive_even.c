35af9497748a44e120a5b223108aae83239d2240
Aarush Aryal <aryalaarush203@gmail.com>
2026-02-19 23:21:19 +0545

    Format code and improve comments in C program

--- positive_odd_positive_even.c
+++ positive_odd_positive_even.c
@@ -3,21 +3,21 @@ int main()
 {
 	int n;
 	printf("Enter a number");
-	//asking the user for a number
+	//Asking the user for a number
 	scanf("%d",&n);
-	//storing the number in the variable n
+	//Storing the number in the variable n
 	if(n<0)
-	//checking if the number is negative
+	//Checking if the number is negative
 	{
 		printf("The number is non positive");
 	}
 	else if(n>0 && n%2==0)
-	//condition for when number is positive and even
+	//Condition for when number is positive and even
 	{
 		printf("The number is positive even");
 	}
 	else if(n>0 && n%2 != 0)
-	//condition for when number is positive and odd
+	//Condition for when number is positive and odd
 	{
 		printf("The number is positive odd");
 	}
