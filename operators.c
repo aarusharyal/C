73da2fa12b458d27b7f8faa26035003608dbfda6
Aarush Aryal <aryalaarush203@gmail.com>
2026-02-26 22:08:55 +0545

    Fix formatting and clean up operators.c

--- operators.c
+++ operators.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
  int main() {
-  int x=5; x++; 			// post increment operater 
+  int x=5; 
+  x++; 			// post increment operater 
   printf("x= %d\n",x); ++x; //pre increment operater 
   printf("x= %d\n",x); x--; //post decrement operater 
   printf("x= %d\n",x); --x; //pre decrement operater 
   printf("x= %d\n",x); 
   return 0; 
   }
-
-
