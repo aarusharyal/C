e00dc80a6c87cae8c1717ba606bbc11011c9c2c4
Aarush Aryal <aryalaarush203@gmail.com>
2026-04-05 11:46:23 +0545

    Add files via upload

---
+++ autoStorageClass.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    auto int i = 10;
+    printf("%d\n", i);
+
+    {
+        auto int i = 20;
+        printf("%d\n", i);
+    }
+
+    printf("%d\n", i);
+    return 0;
+}
