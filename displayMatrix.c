042f2ec28c5943784310d9691c68781ad8906649
Aarush Aryal <aryalaarush203@gmail.com>
2026-04-06 19:36:04 +0545

    Add files via upload

---
+++ displayMatrix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+void display(int zix[2][2]);
+
+void main()
+{
+    int matrix[2][2], i, j;
+    printf("Input matrix elements\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    display(matrix);
+    getch();
+}
+
+void display(int zix[2][2])
+{
+    int i, j;
+    printf("The matrix is\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf(" %d", zix[i][j]);
+        }
+        printf("\n");
+    }
+}
