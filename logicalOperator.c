d2f59b4ce0ac6d6ef22f0aea459fa05d37d3a3d5
Aarush Aryal <aryalaarush203@gmail.com>
2026-04-09 23:09:55 +0545

    Add files via upload

---
+++ logicalOperator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int num1 = 50, num2 = 100;
+    int AND, OR, XOR;
+
+    AND = num1 & num2;
+    OR = num1 | num2;
+    XOR = num1 ^ num2;
+
+    printf("AND => %d\n", AND);
+    printf("OR => %d\n", OR);
+    printf("XOR => %d\n", XOR);
+
+    getch();
+}
